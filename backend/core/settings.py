a0f83447c2391761dce61c06ac91f2e4da794bcb
Marc Jermaine Pontiveros <109291819+marcjermainepontiveros-sun@users.noreply.github.com>
2022-09-14 17:36:29 +0800

    Deployment | Manage Static and Media Files (#2)

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -110,8 +110,35 @@ USE_TZ = env.bool("USE_TZ", default=False)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = env("STATIC_URL")
+# STATIC_URL = env("STATIC_URL")
+USE_S3 = env.bool("USE_S3", default=False)
 
+if USE_S3:
+    # aws settings
+    AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
+    AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+    AWS_DEFAULT_ACL = "public-read"
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+    # s3 static settings
+    AWS_LOCATION = env("AWS_LOCATION")
+    AWS_LOCATION_MEDIA = env("AWS_LOCATION_MEDIA")
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION_MEDIA}/"
+    AWS_S3_SIGNATURE_VERSION = "s3v4"
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = "public-read"
+    AWS_S3_VERIFY = True
+    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+else:
+    STATIC_URL = "/static/"
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
