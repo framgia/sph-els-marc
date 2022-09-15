17e2a14c375dcaedaf4f1780202658849267ef03
Marc Jermaine Pontiveros <marcjermaine.pontiveros@sun-asterisk.com>
2022-09-15 17:46:19 +0800

    Removed patch endpoints, made CategorySerializer a WritableNestedSerializer with Words

--- backend/elearning/serializers.py
+++ backend/elearning/serializers.py
@@ -1,21 +1,7 @@
 from rest_framework import serializers
 from .models import Category, Word
-
-# make a serializer for model Category
-class CategorySerializer(serializers.ModelSerializer):
-    # specify the model to be serialized
-    class Meta:
-        model = Category
-        # specify the fields to be serialized
-        fields = (
-            "id",
-            "category_name",
-            "category_description",
-            "num_items",
-            "date_created",
-            "date_updated",
-        )
-
+from rest_framework.response import Response
+from rest_framework import status
 
 # make a serializer for model Word
 class WordSerializer(serializers.ModelSerializer):
@@ -29,3 +15,65 @@ class WordSerializer(serializers.ModelSerializer):
             "date_created",
             "date_updated",
         )
+
+
+# make a serializer for model Category
+class CategorySerializer(serializers.ModelSerializer):
+    # specify the model to be serialized
+    words = WordSerializer(many=True, read_only=False)
+
+    class Meta:
+        model = Category
+        # specify the fields to be serialized
+        fields = (
+            "id",
+            "category_name",
+            "category_description",
+            "num_items",
+            "date_created",
+            "date_updated",
+            "words",
+        )
+
+    def create(self, validated_data):
+        words_data = validated_data.pop("words")
+        category = Category.objects.create(**validated_data)
+        for word_data in words_data:
+            Word.objects.create(category=category, **word_data)
+        return category
+
+    def update(self, instance, validated_data):
+        words_data = validated_data.pop("words")
+        words = (instance.words).all()
+        words = list(words)
+        instance.category_name = validated_data.get(
+            "category_name", instance.category_name
+        )
+        instance.category_description = validated_data.get(
+            "category_description", instance.category_description
+        )
+        instance.num_items = validated_data.get("num_items", instance.num_items)
+        instance.save()
+        for word_data in words_data:
+            word = words.pop(0)
+            word.word_text = word_data.get("word_text", word.word_text)
+            word.save()
+        return instance
+
+    def partial_update(self, instance, validated_data):
+        words_data = validated_data.pop("words")
+        words = (instance.words).all()
+        words = list(words)
+        instance.category_name = validated_data.get(
+            "category_name", instance.category_name
+        )
+        instance.category_description = validated_data.get(
+            "category_description", instance.category_description
+        )
+        instance.num_items = validated_data.get("num_items", instance.num_items)
+        instance.save()
+        for word_data in words_data:
+            word = words.pop(0)
+            word.word_text = word_data.get("word_text", word.word_text)
+            word.save()
+        return instance
--- backend/elearning/views.py
+++ backend/elearning/views.py
@@ -6,9 +6,11 @@ from .serializers import CategorySerializer, WordSerializer
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all().order_by("-date_created")
     serializer_class = CategorySerializer
+    http_method_names = ["get", "post", "delete"]
 
 
 # make a viewset for model Word
 class WordViewSet(viewsets.ModelViewSet):
     queryset = Word.objects.all().order_by("-date_created")
     serializer_class = WordSerializer
+    http_method_names = ["get", "post", "delete"]
