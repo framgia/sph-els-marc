5faa44ef0c8121f010807f931908bd66294de397
Marc Jermaine Pontiveros <marcjermaine.pontiveros@sun-asterisk.com>
2022-09-15 17:46:19 +0800

    Removed patch endpoints, made CategorySerializer a WritableNestedSerializer with Words

--- backend/elearning/views.py
+++ backend/elearning/views.py
@@ -6,11 +6,11 @@ from .serializers import CategorySerializer, WordSerializer
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all().order_by("-date_created")
     serializer_class = CategorySerializer
-    http_method_names = ["get", "post", "delete"]
+    http_method_names = ["get", "post", "put", "delete"]
 
 
 # make a viewset for model Word
 class WordViewSet(viewsets.ModelViewSet):
     queryset = Word.objects.all().order_by("-date_created")
     serializer_class = WordSerializer
-    http_method_names = ["get", "post", "delete"]
+    http_method_names = ["get", "post", "put", "delete"]
